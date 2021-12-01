3c8520000db7561276e9292eafbbefbc9c63f5ec
KvTsira <kvtsira@gmail.com>
2021-12-01 11:06:09 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,8 +73,13 @@ var fight = function(enemyName) {
     }
   };
 
-// run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
+//function to start a game
+var startGame=function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -85,12 +90,41 @@ for(var i = 0; i < enemyNames.length; i++) {
         // reset enemyHealth before starting new fight
         enemyHealth = 50;
     
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
-    
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
       }
+    else {
+      window.alert ("You have lost your robot in battle! Game Over!");
+      break;
+    }
   }
+
+  //after the loop ends, player is either out of health or enimies to fight , so run the end game function
+  endGame();
+}
+
+//function to end the entire game
+var endGame = function () {
+  //if the player is still alive, player wins!
+  if (playerHealth > 0){
+    window.alert("Great job, you've survived the game! You will now have a score of " + playerMoney + ".");
+  }
+  else{
+    window.alert("The game has now ended. Let's see how you did!");
+  }
+
+  var playAgainConfirm=window.confirm("Would you like to play again?");
+  
+  if (playAgainConfirm){
+    //restart the gane
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiator! Come back soon!");
+  }
+}
+
+//start the game when page loads
+startGame();
 
 
